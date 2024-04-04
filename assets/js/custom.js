dc64f19f522d0861ba41611497f26e442053e4f1
frontenddev344 <frontenddev344@gmail.com>
2024-04-04 16:19:34 +0530

    first commit

---
+++ assets/js/custom.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var acceptCookiesBtn = document.getElementById("accept-cookies");
+    var cookiesPopup = document.getElementById("cookies-popup");
+
+    acceptCookiesBtn.addEventListener("click", function() {
+        cookiesPopup.style.display = "none";
+        // You can add code here to set a cookie to remember the user's choice
+    });
+});
+
+
+$(document).ready(function(){
+    $(".toggle").click(function(){
+      $("body").addClass("menu-toogle");
+    });
+   });
+
+   $(document).ready(function(){
+    $(".close-icn").click(function(){
+      $("body").removeClass("menu-toogle");
+    });
+   });
